3f5f5194087816af88f21173cfc22f9b6d7a4075
Maxamediidgas <maxamediidg.com>
2023-10-26 22:09:46 +0300

    done

--- Foodie/Foodie/User/User.Master.cs
+++ Foodie/Foodie/User/User.Master.cs
@@ -11,7 +11,18 @@ namespace Foodie.User
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if(!Request.Url.AbsoluteUri.ToString().Contains("default.aspx"))
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }else
+            {
+                //Load the control
+                Control SliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
 
+
+                // add control to the panel
+                pnlSliderUC.Controls.Add(SliderUserControl);
+            }
         }
     }
 }
--- Foodie/Foodie/User/User.Master.designer.cs
+++ Foodie/Foodie/User/User.Master.designer.cs
@@ -33,6 +33,15 @@ namespace Foodie.User
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// pnlSliderUC control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlSliderUC;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
